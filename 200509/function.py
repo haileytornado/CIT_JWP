27d58410723f1022c713be63751517846651e4f7
haileytornado <64294646+haileytornado@users.noreply.github.com>
2020-05-09 11:31:58 +0900

    tornado5

---
+++ 200509/function.py
@@ -0,0 +1,14 @@
+def add5(num):
+    result=num+5
+    return result
+print(add5(3))
+
+def mult6(num):
+    result=num*6
+    return result
+print(mult6(add5(7)))
+
+def add(a,b):
+    return a+b
+    print("나는 회오리다")
+print(add(3,9))
